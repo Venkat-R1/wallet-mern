87642d40e4312cc1a4f53e0c86c8152244c54e43
Your Name <you@example.com>
2020-12-24 12:35:18 +0530

    hello

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'client/public')));
+// app.use(express.static(path.join(__dirname, 'client/public')));
 app.use(cors());
 app.use(express.json());
 
